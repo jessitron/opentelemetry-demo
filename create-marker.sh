27a796e711f082262145987742ea85d9fed36910
Jessica Kerr <jessitron@gmail.com>
2023-03-26 23:38:19 +0100

    Put commit hash in marker

--- create-marker.sh
+++ create-marker.sh
@@ -1,4 +1,11 @@
+#!/bin/bash
+
+ref=$(git rev-parse HEAD)
+body=$(echo '{"message":"ref' $ref '", "type":"deploy"}')
+
+echo $body
+
 curl https://api.honeycomb.io/1/markers/frontend-web -X POST  \
     -H "X-Honeycomb-Team: ${HONEYCOMB_API_KEY}"  \
-    -d '{"message":"skaffold run", "type":"deploy"}'
+    -d "$body"
 

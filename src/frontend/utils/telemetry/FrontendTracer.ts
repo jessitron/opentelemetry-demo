a8a66dec14626d79ffd6dc06ca6a2107e5789f05
Jessica Kerr <jessitron@gmail.com>
2024-03-06 12:15:09 -0600

    Trying otel tracing
    but everything is so slow no guarantee this works, i am impatient

--- src/frontend/utils/telemetry/FrontendTracer.ts
+++ src/frontend/utils/telemetry/FrontendTracer.ts
@@ -2,12 +2,6 @@
 // Copyright The OpenTelemetry Authors
 // SPDX-License-Identifier: Apache-2.0
 console.log("jessitron was here ya");
-
-const { NEXT_PUBLIC_OTEL_SERVICE_NAME = '', NEXT_PUBLIC_OTEL_EXPORTER_OTLP_TRACES_ENDPOINT = '', IS_SYNTHETIC_REQUEST = '' } =
-  typeof window !== 'undefined' ? window.ENV : {};
-
-// JESS TODO: insert Honeycomb recommended code instead.
-
 import {
   ConsoleSpanExporter,
   SimpleSpanProcessor,
@@ -17,42 +11,49 @@ import { DocumentLoadInstrumentation } from '@opentelemetry/instrumentation-docu
 import { ZoneContextManager } from '@opentelemetry/context-zone';
 import { registerInstrumentations } from '@opentelemetry/instrumentation';
 
-const provider = new WebTracerProvider();
-provider.addSpanProcessor(new SimpleSpanProcessor(new ConsoleSpanExporter()));
 
-provider.register({
-  // Changing default contextManager to use ZoneContextManager - supports asynchronous operations - optional
-  contextManager: new ZoneContextManager(),
-});
+if (typeof window !== 'undefined') {
 
-// Registering instrumentations
-registerInstrumentations({
-  instrumentations: [new DocumentLoadInstrumentation()],
-  
-});
-// but still do these...
+  const { NEXT_PUBLIC_OTEL_SERVICE_NAME = '', NEXT_PUBLIC_OTEL_EXPORTER_OTLP_TRACES_ENDPOINT = '', IS_SYNTHETIC_REQUEST = '' } =
+    typeof window !== 'undefined' ? window.ENV : {};
 
-// Add session ID:
-// provider.addSpanProcessor(new SessionIdProcessor());
+  const provider = new WebTracerProvider();
+  provider.addSpanProcessor(new SimpleSpanProcessor(new ConsoleSpanExporter()));
 
-// send to the collector:
-// new BatchSpanProcessor(
-//   new OTLPTraceExporter({
-//     url: NEXT_PUBLIC_OTEL_EXPORTER_OTLP_TRACES_ENDPOINT || collectorString || '/v1/traces',
-//   }), {
-//       scheduledDelayMillis : 500
-//     }
+  provider.register({
+    // Changing default contextManager to use ZoneContextManager - supports asynchronous operations - optional
+    contextManager: new ZoneContextManager(),
+  });
+
+  // Registering instrumentations
+  registerInstrumentations({
+    instrumentations: [new DocumentLoadInstrumentation()],
+
+  });
+  // but still do these...
+
+  // Add session ID:
+  // provider.addSpanProcessor(new SessionIdProcessor());
+
+  // send to the collector:
+  // new BatchSpanProcessor(
+  //   new OTLPTraceExporter({
+  //     url: NEXT_PUBLIC_OTEL_EXPORTER_OTLP_TRACES_ENDPOINT || collectorString || '/v1/traces',
+  //   }), {
+  //       scheduledDelayMillis : 500
+  //     }
 
 
-// for my talk, consider implementing something to replace this:
-// new UserInteractionInstrumentation({
-//   eventNames: ['submit', 'click', 'keypress'],
-//   shouldPreventSpanCreation: (eventType, element, span) => {
-//     element['active_span'] = span; // jess does something weird. does this work? yes for clicks, no for submit
-//     span.setAttribute('target.id', element.id);
-//     span.setAttribute('target.className', element.className);
-//     span.setAttribute('target.html', element.outerHTML);
-//   },
-// })
+  // for my talk, consider implementing something to replace this:
+  // new UserInteractionInstrumentation({
+  //   eventNames: ['submit', 'click', 'keypress'],
+  //   shouldPreventSpanCreation: (eventType, element, span) => {
+  //     element['active_span'] = span; // jess does something weird. does this work? yes for clicks, no for submit
+  //     span.setAttribute('target.id', element.id);
+  //     span.setAttribute('target.className', element.className);
+  //     span.setAttribute('target.html', element.outerHTML);
+  //   },
+  // })
+}
 const FrontendTracer = () => { console.log("frontend tracer is a no-op right now") };
 export default FrontendTracer;

64c6001539e084e7fe11a898a53d658d562012b3
Jessica Kerr <jessitron@gmail.com>
2024-04-03 10:49:09 -0500

    deploying

--- src/frontend/pages/_app.tsx
+++ src/frontend/pages/_app.tsx
@@ -1,6 +1,7 @@
 // Copyright The OpenTelemetry Authors
 // SPDX-License-Identifier: Apache-2.0
 
+import '../utils/telemetry/FrontendTracer';
 import '../styles/globals.css';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import App, { AppContext, AppProps } from 'next/app';
@@ -8,7 +9,6 @@ import CurrencyProvider from '../providers/Currency.provider';
 import CartProvider from '../providers/Cart.provider';
 import { ThemeProvider } from 'styled-components';
 import Theme from '../styles/Theme';
-import FrontendTracer from '../utils/telemetry/FrontendTracer';
 
 declare global {
   interface Window {
@@ -18,10 +18,6 @@ declare global {
       NEXT_PUBLIC_OTEL_EXPORTER_OTLP_TRACES_ENDPOINT?: string;
     };
   }
-}
-
-if (typeof window !== 'undefined') {
-  FrontendTracer();
 }
 
 const queryClient = new QueryClient();
--- src/frontend/utils/telemetry/FrontendTracer.ts
+++ src/frontend/utils/telemetry/FrontendTracer.ts
@@ -2,9 +2,11 @@
 import { HoneycombWebSDK } from '@honeycombio/opentelemetry-web';
 import { getWebAutoInstrumentations } from '@opentelemetry/auto-instrumentations-web';
 
-const sdk = new HoneycombWebSDK({
-  apiKey: 'hcaik_01htja3fyhkmj9mf2v1s27ag8wpkeazcvm9rbrzwfpeex8x5dx07rtende',
-  serviceName: 'frontend-web',
-  instrumentations: [getWebAutoInstrumentations()], // add automatic instrumentation
-});
-sdk.start();
+if (typeof window !== 'undefined') {
+  const sdk = new HoneycombWebSDK({
+    apiKey: 'hcaik_01htja3fyhkmj9mf2v1s27ag8wpkeazcvm9rbrzwfpeex8x5dx07rtende',
+    serviceName: 'frontend-web',
+    instrumentations: [getWebAutoInstrumentations()], // add automatic instrumentation
+  });
+  sdk.start();
+}

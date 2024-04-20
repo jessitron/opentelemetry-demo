b17056b214c7435f817273b9dc15d1719837fda1
Jessica Kerr <jessitron@gmail.com>
2024-04-20 13:07:19 -0500

    deploying

--- src/frontend/utils/telemetry/FrontendTracer.ts
+++ src/frontend/utils/telemetry/FrontendTracer.ts
@@ -3,7 +3,10 @@ import { HoneycombWebSDK } from '@honeycombio/opentelemetry-web';
 import { getWebAutoInstrumentations } from '@opentelemetry/auto-instrumentations-web';
 import { SessionIdProcessor } from './SessionIdProcessor';
 
+import { diag, DiagConsoleLogger, DiagLogLevel } from '@opentelemetry/api';
+
 if (typeof window !== 'undefined') {
+  diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.DEBUG);
   console.log("Starting Honeycomb SDK");
   const sdk = new HoneycombWebSDK({
     apiKey: 'hcaik_01htja3fyhkmj9mf2v1s27ag8wpkeazcvm9rbrzwfpeex8x5dx07rtende',
@@ -11,34 +14,35 @@ if (typeof window !== 'undefined') {
     spanProcessor: new SessionIdProcessor(), // add custom span processor
     instrumentations: [getWebAutoInstrumentations()], // add automatic instrumentation
     debug: true,
-    // webVitalsInstrumentationConfig: {
-    //   inp: {
-    //     reportAllChanges: true,
-    //     applyCustomAttributes: (vital, span) => { // this function is required? whatever
-    //       console.log("Received INP", vital.name, vital.value, span.spanContext().traceId);
-    //     },
-    //   },
-    //   cls: {
-    //     reportAllChanges: true,
-    //     applyCustomAttributes: (vital, span) => { // this function is required? whatever
-    //       console.log("Received CLS", vital.name, vital.value, span.spanContext().traceId);
-    //     },
-    //   },
-    //   lcp: {
-    //     reportAllChanges: true,
-    //     applyCustomAttributes: (vital, span) => {
-    //       // from the example
-    //       console.log("Received LCP", vital.name, vital.value, span.spanContext().traceId);
-    //       if (vital.value < 1000) {
-    //         span.setAttribute('lcp.custom_rating', 'good');
-    //       } else if (vital.value < 3000) {
-    //         span.setAttribute('lcp.custom_rating', 'needs-improvement');
-    //       } else {
-    //         span.setAttribute('lcp.custom_rating', 'poor');
-    //       }
-    //     },
-    //   },
-    // },
+    webVitalsInstrumentationConfig: {
+      enabled: true,
+      inp: {
+        reportAllChanges: true,
+        applyCustomAttributes: (vital, span) => { // this function is required? whatever
+          console.log("Received INP", vital.name, vital.value, span.spanContext().traceId);
+        },
+      },
+      cls: {
+        reportAllChanges: true,
+        applyCustomAttributes: (vital, span) => { // this function is required? whatever
+          console.log("Received CLS", vital.name, vital.value, span.spanContext().traceId);
+        },
+      },
+      lcp: {
+        reportAllChanges: true,
+        applyCustomAttributes: (vital, span) => {
+          // from the example
+          console.log("Received LCP", vital.name, vital.value, span.spanContext().traceId);
+          if (vital.value < 1000) {
+            span.setAttribute('lcp.custom_rating', 'good');
+          } else if (vital.value < 3000) {
+            span.setAttribute('lcp.custom_rating', 'needs-improvement');
+          } else {
+            span.setAttribute('lcp.custom_rating', 'poor');
+          }
+        },
+      },
+    },
   });
   sdk.start();
 } else {

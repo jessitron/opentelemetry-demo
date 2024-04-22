9ddb631b37cd39029084517af71b07e0edf51232
Jessica Kerr <jessitron@gmail.com>
2024-04-21 22:28:04 -0500

    Revert "Code as displayed in demo"
    This reverts commit 2dd9b44c7fce22ab81b1f44f1464439fdca546b3.

--- src/frontend/utils/telemetry/FrontendTracer.ts
+++ src/frontend/utils/telemetry/FrontendTracer.ts
@@ -1,9 +1,50 @@
+// Example filename: tracing.js
 import { HoneycombWebSDK } from '@honeycombio/opentelemetry-web';
 import { getWebAutoInstrumentations } from '@opentelemetry/auto-instrumentations-web';
+import { SessionIdProcessor } from './SessionIdProcessor';
 
-const sdk = new HoneycombWebSDK({
-  apiKey: process.env["HONEYCOMB_API_KEY"],
-  serviceName: 'frontend-web',
-  instrumentations: [getWebAutoInstrumentations()],
-});
-sdk.start();
+import { diag, DiagConsoleLogger, DiagLogLevel } from '@opentelemetry/api';
+
+if (typeof window !== 'undefined') {
+  diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.DEBUG);
+  console.log("Starting Honeycomb SDK");
+  const sdk = new HoneycombWebSDK({
+    apiKey: 'hcaik_01htja3fyhkmj9mf2v1s27ag8wpkeazcvm9rbrzwfpeex8x5dx07rtende',
+    serviceName: 'frontend-web',
+    spanProcessor: new SessionIdProcessor(), // add custom span processor
+    instrumentations: [getWebAutoInstrumentations()], // add automatic instrumentation
+    debug: true,
+    webVitalsInstrumentationConfig: {
+      enabled: true,
+      inp: {
+        reportAllChanges: true,
+        applyCustomAttributes: (vital, span) => { // this function is required? whatever
+          console.log("Received INP", vital.name, vital.value, span.spanContext().traceId);
+        },
+      },
+      cls: {
+        reportAllChanges: true,
+        applyCustomAttributes: (vital, span) => { // this function is required? whatever
+          console.log("Received CLS", vital.name, vital.value, span.spanContext().traceId);
+        },
+      },
+      lcp: {
+        reportAllChanges: true,
+        applyCustomAttributes: (vital, span) => {
+          // from the example
+          console.log("Received LCP", vital.name, vital.value, span.spanContext().traceId);
+          if (vital.value < 1000) {
+            span.setAttribute('lcp.custom_rating', 'good');
+          } else if (vital.value < 3000) {
+            span.setAttribute('lcp.custom_rating', 'needs-improvement');
+          } else {
+            span.setAttribute('lcp.custom_rating', 'poor');
+          }
+        },
+      },
+    },
+  });
+  sdk.start();
+} else {
+  console.log("\n   Not loading SDK because we are the server  \n");
+}
